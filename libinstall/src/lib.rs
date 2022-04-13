1942e37c34c17e3c84a7fe832678854bd0017959
Till Wegmueller <toasterson@gmail.com>
2022-04-13 00:11:36 -0300

    Implement commands and parser of commands

--- libinstall/src/lib.rs
+++ libinstall/src/lib.rs
@@ -1,8 +1,19 @@
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+
+pub mod config;
+
 #[cfg(test)]
 mod tests {
+    use crate::config;
+    use crate::config::parse_config_to_instructions;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn initial_test() {
+        let config_file = "zpool-create --ashift=\"12\" mirror c1t0d0s0 c2t0d0s0 c3t0d0s0\nlocale en_US\n";
+        let config_ast = config::parse_config(config_file).unwrap();
+        let config_instructions = parse_config_to_instructions(config_ast);
+        println!("{:?}", config_instructions);
     }
 }
